8b0a26f200d74256968ea3b8407d10a8b290574b
sashaFPV <andrei.cazanov2017@gmail.com>
2022-10-28 16:19:09 +0200

    fix: change dll models

---
+++ ClientWPF/Migrations/202210281403052_ProductImageMigration.cs
@@ -0,0 +1,26 @@
+﻿namespace ClientWPF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ProductImageMigration : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Categories", "Popularity", c => c.Double(nullable: false));
+            AddColumn("dbo.Producers", "Rate", c => c.Double(nullable: false));
+            AddColumn("dbo.Products", "CreationDate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Products", "Quantity", c => c.Int(nullable: false));
+            DropColumn("dbo.Producers", "ProducerRate");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Producers", "ProducerRate", c => c.Double(nullable: false));
+            DropColumn("dbo.Products", "Quantity");
+            DropColumn("dbo.Products", "CreationDate");
+            DropColumn("dbo.Producers", "Rate");
+            DropColumn("dbo.Categories", "Popularity");
+        }
+    }
+}
---
+++ ClientWPF/Migrations/202210281415390_ProductImageRefactorMigration.cs
@@ -0,0 +1,31 @@
+﻿namespace ClientWPF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ProductImageRefactorMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ProductsImages",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Image = c.Binary(nullable: false),
+                        ProductId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Products", t => t.ProductId, cascadeDelete: true)
+                .Index(t => t.ProductId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.ProductsImages", "ProductId", "dbo.Products");
+            DropIndex("dbo.ProductsImages", new[] { "ProductId" });
+            DropTable("dbo.ProductsImages");
+        }
+    }
+}
