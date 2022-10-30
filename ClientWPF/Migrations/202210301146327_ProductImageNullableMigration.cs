826835f713709242d91b4518c14b862a8da44606
sashaFPV <andrei.cazanov2017@gmail.com>
2022-10-30 17:06:14 +0100

    feat: adding products and display all products

---
+++ ClientWPF/Migrations/202210301146327_ProductImageNullableMigration.cs
@@ -0,0 +1,18 @@
+﻿namespace ClientWPF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ProductImageNullableMigration : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.ProductsImages", "Image", c => c.Binary());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.ProductsImages", "Image", c => c.Binary(nullable: false));
+        }
+    }
+}
